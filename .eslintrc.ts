7290f7868eadb7e2f80d370f2772c65316a919ad
Andrew Southern <whoamihealay@gmail.com>
2022-02-11 14:51:58 -0500

    Changed eslintrc and babel.config to typescript

---
+++ .eslintrc.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.ts', '.ts', '.tsx'] }]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
+};
